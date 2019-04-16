66e4b63be4a0e13a3c6730480dc6d909b71ffebc
peter andrew <peter.andrew987@gmail.com>
2019-04-16 22:20:04 +0700

    revert

---
+++ app/Book.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    protected $fillable = ['id','book_name','book_category','book_year','owner_id'];
+
+    public function owner(){
+        return $this->hasMany('App\User');
+    }
+}
--- app/User.php
+++ app/User.php
@@ -19,6 +19,9 @@ class User extends Authenticatable
         'name', 'email', 'password',
     ];
 
+    public function book(){
+        return $this->hasMany('App\Book');
+    }
     /**
      * The attributes that should be hidden for arrays.
      *
